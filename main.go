2584a222a0a1446044f6e825bfb1037b8cf1fd9d
Arsham79 <arsham.ramezani@gmail.com>
2022-09-11 20:42:50 +0430

    refactor: message edited

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
+
+type Message struct {
+	chat int64
+	id   int
+}
 
 func main() {
 	env_err := godotenv.Load(".env")
@@ -23,41 +29,63 @@ func main() {
 	if bot_err != nil {
 		log.Panic(bot_err)
 	}
-	channelId, _ := strconv.ParseInt(os.Getenv("CHANNEL_ID"), 10, 64)
-	messageId, _ := strconv.Atoi(os.Getenv("MESSAGE_ID"))
 
-	send_message(bot, channelId, messageId)
+	data := get_data()
 
+	send_message(bot, data)
+
+}
+func get_data() []Message {
+	channelsId := strings.Split(os.Getenv("CHANNELS_ID"), " ")
+	messagesId := strings.Split(os.Getenv("MESSAGES_ID"), " ")
+	var messages []Message
+	for i := 0; i < len(channelsId); i++ {
+		id, _ := strconv.ParseInt(channelsId[i], 10, 64)
+		chat, _ := strconv.Atoi(messagesId[i])
+		messages = append(messages, Message{id, chat})
+	}
+	return messages
 }
 func create_message(totalSeconds int) string {
 	var hour string
+	var minute string
+
 	if totalSeconds/3600 < 10 {
 		hour = "0" + strconv.Itoa(totalSeconds/3600)
 	} else {
 		hour = strconv.Itoa(totalSeconds / 3600)
 	}
-	var minute string
 	if (totalSeconds%3600)/60 < 10 {
 		minute = "0" + strconv.Itoa((totalSeconds%3600)/60)
 	} else {
 		minute = strconv.Itoa((totalSeconds % 3600) / 60)
 	}
-	result := fmt.Sprintf("%s:%s:00", hour, minute)
+	time := fmt.Sprintf("%s:%s:00", hour, minute)
+	result := fmt.Sprintf("(13 سپتامبر) مطابق با 256 امین روز سال روز جهانی برنامه نویس نام دارد.☕️\nعدد ۲۵۶ بزرگترین توان عدد ۲ کوچکتر از ۳۶۵ که تعداد روزهای یک سال است، می‌باشد.💻\nدر سیستم دودویی کامپیوتری تعداد اعداد منحصربه‌فردی که می‌توان توسط یک بایت تولیدکرد ۲۵۶ تا می‌باشد، که شامل اعداد ۰ تا ۲۵۵ است.\n %s مانده تا روز برنامه نویس", time)
 	return result
 }
-func send_message(bot *tgbotapi.BotAPI, channelId int64, messageId int) {
+func send_message(bot *tgbotapi.BotAPI, data []Message) {
 
 	totalSeconds := (13-time.Now().Day())*86400 - time.Now().Hour()*3600 - time.Now().Minute()*60 - time.Now().Second()
+
 	for {
 		if totalSeconds > 0 {
-			// fmt.Println(totalSeconds)
-			msg := tgbotapi.NewEditMessageText(channelId, messageId, create_message(totalSeconds))
-			bot.Send(msg)
+			for i := 0; i < len(data); i++ {
+				msg := tgbotapi.NewEditMessageCaption(data[i].chat, data[i].id, create_message(totalSeconds))
+				bot.Send(msg)
+			}
 			time.Sleep(time.Minute)
 			totalSeconds -= 60
+
 		} else {
 			break
 		}
+	}
+
+	message := "امروز 22 شهریور (13 سپتامبر) مطابق با 256 امین روز سال روز جهانی برنامه نویس نام دارد.☕️\n\nعدد ۲۵۶ بزرگترین توان عدد ۲ کوچکتر از ۳۶۵ که تعداد روزهای یک سال است، می‌باشد.💻\nدر سیستم دودویی کامپیوتری تعداد اعداد منحصربه‌فردی که می‌توان توسط یک بایت تولیدکرد ۲۵۶ تا می‌باشد، که شامل اعداد ۰ تا ۲۵۵ است.\nروز برنامه نویس رو خدمت شما برنامه نویسان عزیز تبریک میگیم. 🎊🎉"
+	for i := 0; i < len(data); i++ {
+		msg := tgbotapi.NewEditMessageCaption(data[i].chat, data[i].id, message)
+		bot.Send(msg)
 	}
 
 }
