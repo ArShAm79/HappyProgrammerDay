d9aa935e4dc8cd2fe6fcfa4852593f7ede4db6fc
Arsham79 <arsham.ramezani@gmail.com>
2022-09-11 17:38:51 +0430

    refactor: some function added for cleaner code refactor: data loaded from .env file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -22,15 +23,41 @@ func main() {
 	if bot_err != nil {
 		log.Panic(bot_err)
 	}
+	channelId, _ := strconv.ParseInt(os.Getenv("CHANNEL_ID"), 10, 64)
+	messageId, _ := strconv.Atoi(os.Getenv("MESSAGE_ID"))
 
-	totalSeconds := (13-time.Now().Day())*86400 + time.Now().Hour()*3600 + time.Now().Minute()*60 + time.Now().Second()
+	send_message(bot, channelId, messageId)
+
+}
+func create_message(totalSeconds int) string {
+	var hour string
+	if totalSeconds/3600 < 10 {
+		hour = "0" + strconv.Itoa(totalSeconds/3600)
+	} else {
+		hour = strconv.Itoa(totalSeconds / 3600)
+	}
+	var minute string
+	if (totalSeconds%3600)/60 < 10 {
+		minute = "0" + strconv.Itoa((totalSeconds%3600)/60)
+	} else {
+		minute = strconv.Itoa((totalSeconds % 3600) / 60)
+	}
+	result := fmt.Sprintf("%s:%s:00", hour, minute)
+	return result
+}
+func send_message(bot *tgbotapi.BotAPI, channelId int64, messageId int) {
+
+	totalSeconds := (13-time.Now().Day())*86400 - time.Now().Hour()*3600 - time.Now().Minute()*60 - time.Now().Second()
 	for {
 		if totalSeconds > 0 {
-			fmt.Println(totalSeconds)
-			msg := tgbotapi.NewEditMessageText(-1001563456365, 41, fmt.Sprintf("%v:%v:00\n", totalSeconds/3600, (totalSeconds%3600)/60))
+			// fmt.Println(totalSeconds)
+			msg := tgbotapi.NewEditMessageText(channelId, messageId, create_message(totalSeconds))
 			bot.Send(msg)
 			time.Sleep(time.Minute)
 			totalSeconds -= 60
+		} else {
+			break
 		}
 	}
+
 }
